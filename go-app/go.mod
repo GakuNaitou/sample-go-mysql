511380b91320c4fc6e44bf59899e84a7691dcd2a
Naitou Gaku <gaku.naitou@gmail.com>
2021-02-23 22:28:53 +0900

    db接続

--- go-app/go.mod
+++ go-app/go.mod
@@ -2,4 +2,8 @@ module github.com/GakuNaitou/sample-go-mysql
 
 go 1.14
 
-require github.com/labstack/echo/v4 v4.2.0
+require (
+	github.com/labstack/echo/v4 v4.2.0
+	gorm.io/driver/mysql v1.0.4
+	gorm.io/gorm v1.20.12
+)
--- go-app/main.go
+++ go-app/main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+type SampleUser struct {
+	gorm.Model
+	Name string
+}
 
 func main() {
 	e := echo.New()
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(mysql:3306)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_DB"),
+	)
+
+	sqlDB, err := sql.Open(os.Getenv("MYSQL_HOST"), dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	gormDB, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	gormDB.AutoMigrate(&SampleUser{})
+	gormDB.Create(&SampleUser{Name: "sample name"})
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		var sampleUser SampleUser
+		gormDB.First(&sampleUser)
+
+		return c.String(http.StatusOK,
+			fmt.Sprintf("%s", sampleUser.Name),
+		)
 	})
+
 	e.Logger.Fatal(e.Start(":9000"))
 }
