dc82b39238912dc0124949dd64293c8f82ded1f2
Naitou Gaku <gaku.naitou@gmail.com>
2021-03-03 08:12:59 +0900

    main.goでdbとtemplateをコメントアウト

--- go-app/main.go
+++ go-app/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"html/template"
 	"io"
 	"net/http"
-	"os"
 
 	"github.com/labstack/echo/v4"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -27,47 +23,47 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 }
 
 func main() {
-	t := &Template{
-		templates: template.Must(template.ParseGlob("views/*.html")),
-	}
+	// t := &Template{
+	// 	templates: template.Must(template.ParseGlob("views/*.html")),
+	// }
 	e := echo.New()
-	e.Renderer = t
+	// e.Renderer = t
 
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(mysql:3306)/%s?charset=utf8&parseTime=True&loc=Local",
-		os.Getenv("MYSQL_USER"),
-		os.Getenv("MYSQL_PASSWORD"),
-		os.Getenv("MYSQL_DB"),
-	)
-	sqlDB, err := sql.Open(os.Getenv("MYSQL_HOST"), dsn)
-	if err != nil {
-		panic(err)
-	}
-	gormDB, err := gorm.Open(mysql.New(mysql.Config{
-		Conn: sqlDB,
-	}), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
+	// dsn := fmt.Sprintf(
+	// 	"%s:%s@tcp(mysql:3306)/%s?charset=utf8&parseTime=True&loc=Local",
+	// 	os.Getenv("MYSQL_USER"),
+	// 	os.Getenv("MYSQL_PASSWORD"),
+	// 	os.Getenv("MYSQL_DB"),
+	// )
+	// sqlDB, err := sql.Open(os.Getenv("MYSQL_HOST"), dsn)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// gormDB, err := gorm.Open(mysql.New(mysql.Config{
+	// 	Conn: sqlDB,
+	// }), &gorm.Config{})
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	gormDB.AutoMigrate(&SampleUser{})
+	// gormDB.AutoMigrate(&SampleUser{})
 
 	e.GET("/", func(c echo.Context) error {
-		var sampleUsers []SampleUser
-		gormDB.Find(&sampleUsers)
+		// var sampleUsers []SampleUser
+		// gormDB.Find(&sampleUsers)
 
-		ViewData := struct {
-			SampleUsers []SampleUser
-		}{
-			SampleUsers: sampleUsers,
-		}
+		// ViewData := struct {
+		// 	SampleUsers []SampleUser
+		// }{
+		// 	SampleUsers: sampleUsers,
+		// }
 
-		return c.Render(http.StatusOK, "main.html", ViewData)
+		return c.Render(http.StatusOK, "main.html", "hoge")
 	})
 
 	e.POST("/", func(c echo.Context) error {
-		sampleUserName := c.FormValue("name")
-		gormDB.Create(&SampleUser{Name: sampleUserName})
+		// sampleUserName := c.FormValue("name")
+		// gormDB.Create(&SampleUser{Name: sampleUserName})
 
 		return c.Redirect(http.StatusFound, "/")
 	})
